7d4572283d8729d3546a17093fe1a878bc11d740
victorvenuci-arch <victorvenuci@gmail.com>
2026-03-01 15:39:36 -0300

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+# Importa banco
+from services.database_core import engine, Base
+
+# Importa modelos (IMPORTANTE para criar tabela)
+from data_models.user import User
+
 app = FastAPI(
     title="Dracco Backend",
     version="1.0.0"
 )
 
-# CORS aberto (produção depois ajustamos)
+# Cria todas as tabelas registradas no Base
+Base.metadata.create_all(bind=engine)
+
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
