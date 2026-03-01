a05f82bc4dbdd82c4cfdfcb781c2b1eca8524558
victorvenuci-arch <victorvenuci@gmail.com>
2026-03-01 19:25:57 -0300

    schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel, EmailStr
 
+
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
     password: str
 
+
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
 
 class UserResponse(BaseModel):
     id: int
