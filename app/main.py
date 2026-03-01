a7de0e35be8a8b7f478485a4d9e6a025dbe4533c
victorvenuci-arch <victorvenuci@gmail.com>
2026-03-01 16:43:46 -0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
+from fastapi import FastAPI
+from .database import engine
+from .models import Base
+from .routers import users
 
+app = FastAPI(title="Dracco Backend Pro")
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(users.router)
+
+@app.get("/")
+def root():
+    return {"message": "API is running 🚀"}
