8032c1a6afb7bd138a4239d9877431c777686120
victorvenuci-arch <victorvenuci@gmail.com>
2026-03-01 17:29:35 -0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,2 @@
-from database import engine
-from models import Base
-
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
+from app.database import engine
+from app.models import Base
